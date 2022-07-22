6455fa0c364211b3689cb530bd8e5ff4e01e4b11
Kirill Kurko <58859242+kirillkurko@users.noreply.github.com>
2022-07-22 09:53:17 +0200

    Time: 80 ms (77.21%), Space: 45.2 MB (16.75%) - LeetHub

---
+++ 941-valid-mountain-array/941-valid-mountain-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var validMountainArray = function(arr) {
+    let N = arr.length;
+	let i = 0;
+    
+	while (i + 1 < N && arr[i] < arr[i + 1]) {
+		i += 1;
+	}
+
+	if (i === 0 || i === N - 1) {
+		return false;
+	}
+
+	while (i + 1 < N && arr[i] > arr[i + 1]) {
+		i += 1;
+	}
+
+	return i === N - 1;
+};
