4643e654e18da1eefb767aae74b7445460e79eef
Kirill Kurko <58859242+kirillkurko@users.noreply.github.com>
2022-07-22 08:59:25 +0200

    Time: 114 ms (66.67%), Space: 45.8 MB (16.67%) - LeetHub

--- 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.ts
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.ts
@@ -1,21 +1,22 @@
 function checkAlmostEquivalent(word1: string, word2: string): boolean {
-  for (let i = 0; i < word1.length; ++i) {
-    const firstCount = word1.split(word1[i]).length - 1;
-    const secondCount = word2.split(word1[i]).length - 1;
+    const letters = new Array(26).fill(0);
 
-    if (Math.abs(firstCount - secondCount) > 3) {
-      return false;
-    }
-  }
+    const aChar = "a".charCodeAt(0);
     
-  for (let i = 0; i < word2.length; ++i) {
-    const firstCount = word1.split(word2[i]).length - 1;
-    const secondCount = word2.split(word2[i]).length - 1;
-
-    if (Math.abs(firstCount - secondCount) > 3) {
-      return false;
+    for (let i = 0; i < word1.length; ++i) {
+        letters[word1.charCodeAt(i) - aChar] += 1;
+        letters[word2.charCodeAt(i) - aChar] -= 1;
     }
-  }
-
-  return true;
+    
+    console.log(letters[0]);
+    
+    console.log(aChar - word1[0].charCodeAt(0));
+    
+    for (let i = 0; i < 26; ++i) {
+        if (letters[i] > 3 || letters[i] < -3) {
+            return false;
+        }
+    }
+    
+    return true;
 }
