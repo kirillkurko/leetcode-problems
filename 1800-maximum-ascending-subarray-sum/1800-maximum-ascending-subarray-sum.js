fa7ee26347430c276e07c52f36d083757857c04b
Kirill Kurko <58859242+kirillkurko@users.noreply.github.com>
2022-07-27 11:06:43 +0200

    Time: 97 ms (30.77%), Space: 42.2 MB (32.87%) - LeetHub

--- 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.js
+++ 1800-maximum-ascending-subarray-sum/1800-maximum-ascending-subarray-sum.js
@@ -6,7 +6,7 @@ var maxAscendingSum = function(nums) {
     let sum = nums[0];
     let res = sum;
     
-    for (let i = 1; i < nums.length; i++) {
+    for (let i = 1; i < nums.length; ++i) {
         if (nums[i] > nums[i - 1]) {
             sum += nums[i];
             res = Math.max(res, sum);
