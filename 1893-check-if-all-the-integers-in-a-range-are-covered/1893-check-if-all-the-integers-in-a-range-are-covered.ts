4796496e0b08d0964a8f0655cc980babda46d069
Kirill Kurko <58859242+kirillkurko@users.noreply.github.com>
2022-08-12 00:26:39 +0200

    Time: 99 ms (66.67%), Space: 44.9 MB (46.67%) - LeetHub

---
+++ 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.ts
@@ -0,0 +1,10 @@
+function isCovered(ranges: number[][], left: number, right: number): boolean {
+    for (let i = left; i <= right; ++i) {
+        const find = ranges.find(r => i >= r[0] && i <= r[1]);
+        if (!find) {
+            return false;
+        }
+    }
+    
+    return true;
+};
