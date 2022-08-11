7c266bdb0ecc52366045d7ae7417bd27e72c6c61
Kirill Kurko <58859242+kirillkurko@users.noreply.github.com>
2022-08-11 17:17:25 +0200

    Time: 103 ms (31.58%), Space: 45.5 MB (15.79%) - LeetHub

---
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.ts
@@ -0,0 +1,16 @@
+function findSpecialInteger(arr: number[]): number {
+    let count = 0;
+    
+    for (let i = 0; i < arr.length; ++i) {
+        count += 1;
+        
+        if (count > arr.length / 4) {
+            return arr[i];
+        }
+        
+        
+        if (arr[i + 1] !== arr[i]) {
+            count = 0;
+        }
+    }
+};
