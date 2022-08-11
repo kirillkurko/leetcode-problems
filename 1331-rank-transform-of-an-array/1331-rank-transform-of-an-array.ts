538745be80e4532b6fb4c123961a866922131153
Kirill Kurko <58859242+kirillkurko@users.noreply.github.com>
2022-08-11 17:10:27 +0200

    Time: 256 ms (90.00%), Space: 64 MB (85.00%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.ts
@@ -0,0 +1,16 @@
+function arrayRankTransform(arr: number[]): number[] {
+    const sorted = [...new Set(arr)].sort((a, b) => a - b);
+    const map = new Map();
+    
+    for (let i = 0; i < sorted.length; i++) {
+        if (!map.has(sorted[i])) {
+            map.set(sorted[i], i + 1);
+        }
+    }
+    
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = map.get(arr[i]);
+    }
+    
+    return arr;
+};
