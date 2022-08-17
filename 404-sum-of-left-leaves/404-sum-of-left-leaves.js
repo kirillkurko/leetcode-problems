26bce1fabd905dda706b25913bdd9a1afcaab2b1
Kirill Kurko <58859242+kirillkurko@users.noreply.github.com>
2022-08-17 08:31:08 +0300

    Time: 68 ms (92.90%), Space: 44.4 MB (53.91%) - LeetHub

---
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(root) {
+    let sum = 0;
+    
+    if (!root) {
+        return 0;
+    }
+    
+    if (root.left && !root.left.left && !root.left.right) {
+        sum += root.left.val;
+    } 
+    
+    sum += sumOfLeftLeaves(root.right);
+    sum += sumOfLeftLeaves(root.left);
+    
+    return sum;
+};
