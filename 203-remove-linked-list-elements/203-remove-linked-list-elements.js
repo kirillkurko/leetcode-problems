37e3d929e40f860a5bc4016b5d98a14e0543e9d3
Kirill Kurko <58859242+kirillkurko@users.noreply.github.com>
2022-08-24 08:14:32 +0300

    Time: 78 ms (96.35%), Space: 46.2 MB (86.44%) - LeetHub

---
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    
+    let curr = head;
+    
+    while (curr && curr.next) {
+        if (curr.next.val === val) {
+            curr.next = curr.next.next;
+        }
+        
+        if (curr.next && curr.next.val !== val) {
+            curr = curr.next;
+        }
+    }
+    
+    while (head && head.val === val) {
+        head = head.next;
+    }
+    
+    return head;
+};
