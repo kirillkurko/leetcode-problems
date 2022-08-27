cf4a35066e17dad2f1875ecdb9096382b728aafc
Kirill Kurko <58859242+kirillkurko@users.noreply.github.com>
2022-08-27 14:53:06 +0300

    Time: 380 ms (24.47%), Space: 100.3 MB (6.05%) - LeetHub

---
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+    if (!root) {
+        return 0;
+    }
+    
+    if (root.left === null) {
+        return 1 + minDepth(root.right);
+    } else if (root.right === null) {
+        return 1 + minDepth(root.left);
+    }
+    
+    return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+};
