78f0b765d2192aec4ea74d0acfc8acd9991ad8ba
Kirill Kurko <58859242+kirillkurko@users.noreply.github.com>
2022-08-29 09:06:13 +0300

    Time: 370 ms (75.00%), Space: 72.8 MB (52.94%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} spells
+ * @param {number[]} potions
+ * @param {number} success
+ * @return {number[]}
+ */
+var successfulPairs = function(spells, potions, success) {
+    const res = [];
+    
+    potions.sort((a, b) => a - b);
+    
+    for (const spell of spells) {
+        let left = 0;
+        let right = potions.length - 1;
+        
+        while(left < right) {
+            const middle = (left + right) >> 1;
+            
+            if (potions[middle] * spell < success) {
+                left = middle + 1;
+            } else {
+                right = middle;
+            }
+        }
+		
+        if (potions[left] * spell < success) {
+            res.push(potions.length - left - 1);
+        } else {
+            res.push(potions.length - right);
+        }
+    }
+    
+    return res;
+};
