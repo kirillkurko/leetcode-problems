1795eb25c676e56884e4f9cfe29b45dc5534a8f0
Kirill Kurko <58859242+kirillkurko@users.noreply.github.com>
2022-08-31 11:04:10 +0300

    Time: 98 ms (47.17%), Space: 43.1 MB (67.51%) - LeetHub

---
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.js
@@ -0,0 +1,14 @@
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function(letters, target) {
+     for (let i = 0; i < letters.length; ++i) {
+        if (letters[i].charCodeAt(0) > target.charCodeAt(0)) {
+            return letters[i];
+        }
+    }
+    
+    return letters[0];
+};
