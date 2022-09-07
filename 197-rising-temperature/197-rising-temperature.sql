12dbd48a42342063db9d15908473497f8285296d
Kirill Kurko <58859242+kirillkurko@users.noreply.github.com>
2022-09-07 10:19:31 +0300

    Time: 456 ms (76.18%), Space: 0B (100.00%) - LeetHub

---
+++ 197-rising-temperature/197-rising-temperature.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT Weather.Id FROM Weather
+JOIN Weather AS w
+ON w.RecordDate = SUBDATE(Weather.RecordDate, 1)
+WHERE Weather.Temperature > w.Temperature ;
