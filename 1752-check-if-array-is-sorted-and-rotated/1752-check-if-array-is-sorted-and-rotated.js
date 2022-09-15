e0d337da16a1a53ee8d3975b271b8acc36cadbe7
Kirill Kurko <58859242+kirillkurko@users.noreply.github.com>
2022-09-15 20:20:24 +0300

    Time: 105 ms (25.40%), Space: 42.1 MB (52.38%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var check = function(nums) {
+    let count = 0;
+    
+    for (let i = 0 ; i < nums.length; ++i) {
+        if(nums[i] > nums[(i+1) % nums.length]) {
+            count += 1
+        }
+    }
+	
+    return count <= 1;
+};
